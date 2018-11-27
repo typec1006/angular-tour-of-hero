40dff7eacf651b4be8144533abacd7d5bce8a69f
Jun Miyazaki <miyazaki@mba.co.jp>
2018-11-27 17:05:27 +0900

    modified / no.3 master/detail component

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,15 +10,15 @@ import { Hero } from '../hero';
 export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
-
   selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-  
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
